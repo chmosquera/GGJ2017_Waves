cd3f080ec0eb47835687104e56d71b94d79a3f33
chmosquera <ckaith97@yahoo.com>
2017-01-21 16:36:29 -0800

    a lotta stuff board manager

--- Assets/Scripts/CameraFollow.cs
+++ Assets/Scripts/CameraFollow.cs
@@ -4,15 +4,31 @@ using System.Collections;
 public class CameraFollow : MonoBehaviour {
 
 	// Max/Min points the camera is bounded by
-	[SerializeField] private float xMax, yMax, xMin, yMin;
+	public float xMax, yMax, xMin, yMin;
+	private GameManager gameManager;
 
 	private Transform target;
 
 	// Use this for initialization
 	void Start () {
 		target = GameObject.Find ("Player").transform;
+		gameManager = transform.parent.gameObject.GetComponent<GameManager>();
+
+
+		xMax = gameManager.width;
+		yMax = gameManager.height;
+		xMin = 0;
+		yMin = 0;
+
+		/*
+		gameManager = Instantiate (gameManager).GetComponent<GameManager>(); //, new Vector3 (transform.position.x, transform.position.y, 0f), Quaternion.identity) as GameObject;
+		boardManager = GetComponent<BoardManager> ();
+		*/
 	}
-	
+
+	void Update() {
+	}
+
 	// Update is called once per frame
 	void LateUpdate () {
 		// transforms the position of the camera to the position of the player
